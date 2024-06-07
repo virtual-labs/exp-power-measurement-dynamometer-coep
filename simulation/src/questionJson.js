610928e3cd16385e077656d947c613ebc74c2ef3
vleadadmin <admin@vlabs.ac.in>
2024-06-07 09:51:25 +0000

    https://virtual-labs.github.io/virtual-labs/exp-power-measurement-dynamometer-coep click on the link to test your code.

---
+++ simulation/src/questionJson.js
@@ -0,0 +1,143 @@
+
+
+QuestionsJSON = {
+		"data": {
+			"SEC": [{
+
+				"QUES": [{
+					
+					"QC": "Which of the following is a characteristic feature of a dynamometer?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "It can measure torque"
+					}, {
+						"ANSID": false,
+						"content": "It can act as a speedometer"
+					}, {
+						"ANSID": false,
+						"content": "It can measure the balancing force"
+					},{
+						"ANSID": true,
+						"content": "It can measure frictional resistance "
+					}]
+				}]
+			}, {
+
+				"QUES": [{
+					
+					"QC": "For a rope brake dynamometer, the flywheel is cooled with soapy water because________ ",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Energy supplied is more"
+					}, {
+						"ANSID": false,
+						"content": "Energy is provided by the motor"
+					}, {
+						"ANSID": true,
+						"content": "Energy is absorbed by the dynamometer"
+					}, {
+						"ANSID": false,
+						"content": " Energy is used to do work "
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": " For high speed engines, the cam follower should move with",
+					"ANS": [{
+						"ANSID": true,
+						"content": "cyclodial motion"
+					}, {
+						"ANSID": false,
+						"content": "uniform velocity "
+					}, {
+						"ANSID": false,
+						"content": "simple harmonic motion"
+					}, {
+						"ANSID": false,
+						"content": "uniform acceleration and retardation"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "In power transmission equation, P=2ΠNT/60×1000",
+					"ANS": [{
+						"ANSID": true,
+						"content": "P is in kw and T is mean torque"
+					}, {
+						"ANSID": false,
+						"content": "P is in kw and T is maximum torque"
+					}, {
+						"ANSID": false,
+						"content": "P is in NM/sec and T is mean torque"
+					}, {
+						"ANSID": false,
+						"content": "P is in NM/sec and T is maximum torque"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Which property is not required for shaft materials? ",
+					"ANS": [{
+						"ANSID": false,
+						"content": "High shear and tensile strength"
+					}, {
+						"ANSID": false,
+						"content": "Good machinability"
+					}, {
+						"ANSID": true,
+						"content": "Good castability"
+					}, {
+						"ANSID": false,
+						"content": "High fatigue strength"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Two shafts in torsion will have equal strength if",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Only diameter of the shafts is same"
+					}, {
+						"ANSID": true,
+						"content": "Only torque transmitting capacity of the shaft is same"
+					}, {
+						"ANSID": false,
+						"content": "Only angle of twist of the shaft is same"
+					}, {
+						"ANSID": false,
+						"content": "Only material of the shaft is same"
+					}]
+					
+				}]
+			}
+			]
+		}
+
+
+	}
+
+
+
+var ANSWERJSON = {
+			
+				"0" : "All the points mentioned",
+				"1" : "Set the standard output for the required limits of process parameter",
+				"2" : "Set majority of the points of the transmitter output in the operating region",
+				"3" : "Pump discharge flow and Outlet flow",
+				"4" : "Nothing is required to be changed",
+				"5" : "Nil"
+				
+			
+}
